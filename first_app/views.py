4f196f65516d4d5335aed47e4dd1b48d18d8e633
Skillvendor <skillvendor193@gmail.com>
2017-10-01 11:35:32 +0300

    restructured repository

--- first_app/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def index(request):
-  my_dict = {'insert_me':"Hiiiii views.py mueeeee !"}
-  return render(request, 'first_app/index.html', context=my_dict)
